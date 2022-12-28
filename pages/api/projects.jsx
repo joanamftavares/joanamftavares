42880a0a26f1f78f355422d758306bf9222d2f35
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2022-12-28 20:40:01 +0000

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -3,15 +3,15 @@ export default async (req, res) => {
     {
       id: 1,
       image: "https://media.discordapp.net/attachments/737348411568685066/954502826791428136/codeshare_logo.png",
-      name: "Code Share",
-      description: "Discord türkiyenin en büyük discord bot komutları arşivi ve en büyük discord botu uptime, hostlayıcı websitesi. Code Share veritabanında discord botlarına özgü yüzlerce discord bot komutu bulunmaktadır ve kendi discord botunuzu yaratmanıza yardımcı olur.",
+      name: "project 1",
+      description: "project 1 description",
       link: "https://codeshare.me",
     },
     {
       id: 2,
       image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "Youtube Channel",
-      description: "I have always considered myself a problem solver, for every problem that might appear, I will always find a solution to deal with it. During my academic journey, I became fascinated with transforming simple data into a world of knowledge and I am eager to feel the same way while facing new challenges in the next stage of my career",
+      name: "project 2",
+      description: "project 2 description",
       link: "https://youtube.com/UmutBayraktarYT",
     }
   ];
