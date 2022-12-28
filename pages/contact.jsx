ea70013ad40fb94928e07f6cff2e669a67b64a77
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2022-12-28 20:46:45 +0000

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,32 +10,14 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Contact Me</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
+        <p className="text-3xl text-white font-semibold text-center">contact me</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">where can you find me?.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-                <a target="_blank" rel="norefferer" href=" https://instagram.com/umutxyp/">
-                    <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
-                    </div>
-                </a>
-                <a target="_blank" rel="norefferer" href="https://facebook.com/umutxyp/">
-                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-facebook fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
-                    </div>
-                </a>
-                <a target="_blank" rel="norefferer" href="https://discord.com/users/615029465726320654">
-                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">Umut#6070</p>
-                    </div>
-                </a>
-                <a href="mailto:umutbayraktar55@gmail.com">
+                <a href="mailto:joanamftavares@gmail.com">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">umutbayraktar55@hotmail.com</p>
+                        <p className="font-semibold text-xl">joanamftavares@gmail.com</p>
                     </div>
                 </a>
             </div>
