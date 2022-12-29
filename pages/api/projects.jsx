eb3efa3d35916ae403a895ab55c1928222bf25a6
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2022-12-29 14:06:16 +0000

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -2,7 +2,7 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://ibb.co/LnCYnfR",
+      image: "https://cdn.discordapp.com/attachments/919966397524738091/1058022852600799312/4bf029229e9f4ec241117cd42c39aceb89c2c81839e99f001249a448.png",
       name: "investments 4 some",
       description: "app developed during first year of my master's degree. it aims to educate the user on how to invest and how to analyze usual technical indicators",
       link: "https://investments4some-n1xe.onrender.com/",
