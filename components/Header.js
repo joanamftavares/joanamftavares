628ba35c0a42e34b942ff18ba464f22b606df267
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2022-12-29 15:24:26 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
     return <>
         <div className="w-full border-b-2 border-neutral-800/20 pb-2">
             <div className="flex flex-col md:flex-row w-full items-center md:justify-between">
-                <p className="font-semibold font-Poppins text-xl">Joana Tavares - Portfolio</p>
+                <p className="font-semibold font-Poppins text-xl text-green">Joana Tavares - Portfolio</p>
                 <div className="flex items-center space-x-2">
                     {items.map(item => (
                         <a key={item.link} href={item.link} target="_blank" rel="noreferrer" className="flex items-center justify-center hover:bg-neutral-700/20 rounded-xl transition-all duration-150 p-2 px-3">
@@ -29,7 +29,7 @@ const Header = () => {
             <div className="flex justify-center md:justify-start items-center space-x-4 py-2">
                 {navItems.map(item => (
                     <Link key={item.label} href={item.href}>
-                        <a className={`flex items-center justify-center green/50-text cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${router.asPath === item.href && 'text-white/100'}`}>
+                        <a className={`flex items-center justify-center text-white/50 cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${router.asPath === item.href && 'text-white/100'}`}>
                             <i className={`${router.asPath === item.href ? item.active : item.icon} mr-2`} />{item.label}
                         </a>
                     </Link>
