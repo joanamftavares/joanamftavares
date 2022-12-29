dbda1cc9854cb114513bb9601fa409d8cb41b00b
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2022-12-29 14:14:34 +0000

    Update projects.jsx

--- pages/api/projects.jsx
+++ pages/api/projects.jsx
@@ -4,15 +4,15 @@ export default async (req, res) => {
       id: 1,
       image: "https://cdn.discordapp.com/attachments/919966397524738091/1058022852600799312/4bf029229e9f4ec241117cd42c39aceb89c2c81839e99f001249a448.png",
       name: "investments 4 some",
-      description: "app developed during first year of my master's degree. it aims to educate the user on how to invest and how to analyze usual technical indicators",
+      description: "app developed during first year of my master's degree. it aims to educate the user on how to invest and how to analyze usual technical indicators.",
       link: "https://investments4some-n1xe.onrender.com/",
     },
     {
       id: 2,
-      image: "https://media.discordapp.net/attachments/737348411568685066/999563078079361074/YT_AVATAR_NET.jpg",
-      name: "project 2",
-      description: "project 2 description",
-      link: "https://youtube.com/UmutBayraktarYT",
+      image: "https://cdn.discordapp.com/attachments/919966397524738091/1058025165923041351/image.png",
+      name: "bi4all - dashboard",
+      description: "using power bi, we developed a dashboard with the analysis of multiple departments in bi4all. we used multiple techniques and KPI's to help provide insights/solutions.",
+      link: "https://app.powerbi.com/view?r=eyJrIjoiNGM3ZDAwZmMtNzhiNi00ZGUyLWJmN2QtZjg3MTA2ODc2ZjRkIiwidCI6ImU0YmQ2OWZmLWU2ZjctNGMyZS1iMjQ3LTQxYjU0YmEyNDkwZSIsImMiOjh9",
     }
   ];
   res.status(200).json(data);
