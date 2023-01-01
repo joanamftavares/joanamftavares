7455b9211a9d21da5b3e9068f4e586583d0b7814
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2023-01-01 13:57:42 +0000

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,7 @@ export default function Home() {
                           <i className={`fa fa-circle text-online mr-2`} />Online
                     </span>
                   </Tippy>
+                <br></br>
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
