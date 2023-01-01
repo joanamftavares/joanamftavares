46d83889caf51cad26864fdd57d0804704544f42
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2023-01-01 13:27:10 +0000

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -27,16 +27,14 @@ const Header = () => {
                 </div>
             </div>
             <div className="flex justify-center md:justify-start items-center space-x-4 py-2">
-                <span style={{ color:'#AAF0C1' }}</span>
-                    {navItems.map(item => (
-                        <Link key={item.label} href={item.href}>
-                            <a className={`flex items-center justify-center text-white/50 cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${router.asPath === item.href && 'text-white/100'}`}>
-                             <i className={`${router.asPath === item.href ? item.active : item.icon} mr-2`} />{item.label}
-                            </a>
-                        </Link>
-                    ))}
+                {navItems.map(item => (
+                    <Link key={item.label} href={item.href}>
+                        <a className={flex items-center justify-center text-white/50 cursor-pointer hover:text-white/100 rounded-xl transition-all duration-150 ${router.asPath === item.href && 'text-white/100'}}>
+                            <i className={${router.asPath === item.href ? item.active : item.icon} mr-2} />{item.label}
+                        </a>
+                    </Link>
+                ))}
             </div>
-        </div>
     </>
 }
 
