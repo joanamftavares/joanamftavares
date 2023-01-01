17f3a7c6e6834fddbed4061772b41b813b6d7185
joanamftavares <90759356+joanamftavares@users.noreply.github.com>
2023-01-01 13:47:08 +0000

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,13 +10,19 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">contact me</p>
+        <span style={{ color:'#AAF0C1' }}  className="text-3xl text-white font-semibold text-center">find me at</span>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
                 <a href="mailto:joanamftavares@gmail.com">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
                         <p className="font-semibold text-xl">joanamftavares@gmail.com</p>
+                    </div>
+                </a>
+                <a target="_blank" rel="norefferer" href="https://www.linkedin.com/in/joanamftavares/">
+                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
+                        <i className="fab fa-linkedin fa-2x" />
+                        <p className="font-semibold text-xl">Joana Tavares</p>
                     </div>
                 </a>
             </div>
